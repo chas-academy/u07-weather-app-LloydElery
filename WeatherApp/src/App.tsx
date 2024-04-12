39bf6a077b919238628f11dd5b89064034634bb7
LloydElery <dennisgooglar@gmail.com>
2024-04-12 14:17:57 +0200

    First stages, lofi!

--- WeatherApp/src/App.tsx
+++ WeatherApp/src/App.tsx
@@ -1,35 +1,174 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const searchterm = ""; // country/city ...q={country}
+  const limit = ""; // number of location in the response &limit={limit}
+  const zip = ""; // zipcode ...zip?zip={zip}
+  const appId = "164e612402e8456b68fbfabfc8c7ff68";
+  const url = `http://api.openweathermap.org/geo/1.0/direct?q=${searchterm}&limit=${limit}&appid=${appId}`;
+
+  // http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=164e612402e8456b68fbfabfc8c7ff68
+  const [count, setCount] = useState(0);
+
+  const weak = [
+    "Monday",
+    "Teusday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {weak.map((day: string) => (
+          <p>{day}</p>
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      {/* Main container */}
+      <div className="flex flex-col border-2 border-black h-screen m-2">
+        {/* Header container */}
+        <div className="border-2 m-2 w-auto border-black flex h-28 text-lg justify-evenly content-center items-center">
+          {/* Image container */}
+          <div className="border-2 border-black w-10 h-10">
+            <img
+              className="w-full h-full content-center items-center"
+              src=""
+              alt=""
+            />
+          </div>
+          {/* Location Name container */}
+          <div>
+            <h2 className="w-full h-full content-center items-center">
+              Stockholm
+            </h2>
+          </div>
+          {/* Current Temp container */}
+          <div className="border-2 border-black w-10 h-10">
+            <strong className="w-full h-full content-center items-center">
+              30c
+            </strong>
+          </div>
+        </div>
+        {/* Tomorrow container */}
+        <div className="border-2 m-2 w-auto border-black flex h-28 text-lg justify-evenly content-center items-center">
+          {/* Day container */}
+          <div>
+            <h3>Friday</h3>
+          </div>
+          {/* Image container */}
+          <div className="border-2 border-black w-10 h-10">
+            <img
+              className="w-full h-full content-center items-center"
+              src=""
+              alt=""
+            />
+          </div>
+
+          {/* Min-Max temp container */}
+          <div>
+            <strong className="w-full h-full content-center items-center">
+              20c - 30c
+            </strong>
+          </div>
+          {/* Medium Temp container */}
+          <div className="flex border-2 border-black w-10 h-10 ">
+            <strong className="w-full h-full content-center items-center">
+              25c
+            </strong>
+          </div>
+        </div>
+        {/* Tomorrow container */}
+        <div className="border-2 m-2 w-auto border-black flex h-28 text-lg justify-evenly content-center items-center">
+          {/* Day container */}
+          <div>
+            <h3>Friday</h3>
+          </div>
+          {/* Image container */}
+          <div className="border-2 border-black w-10 h-10">
+            <img
+              className="w-full h-full content-center items-center"
+              src=""
+              alt=""
+            />
+          </div>
+
+          {/* Min-Max temp container */}
+          <div>
+            <strong className="w-full h-full content-center items-center">
+              20c - 30c
+            </strong>
+          </div>
+          {/* Medium Temp container */}
+          <div className="flex border-2 border-black w-10 h-10 ">
+            <strong className="w-full h-full content-center items-center">
+              25c
+            </strong>
+          </div>
+        </div>
+        {/* Tomorrow container */}
+        <div className="border-2 m-2 w-auto border-black flex h-28 text-lg justify-evenly content-center items-center">
+          {/* Day container */}
+          <div>
+            <h3>Friday</h3>
+          </div>
+          {/* Image container */}
+          <div className="border-2 border-black w-10 h-10">
+            <img
+              className="w-full h-full content-center items-center"
+              src=""
+              alt=""
+            />
+          </div>
+
+          {/* Min-Max temp container */}
+          <div>
+            <strong className="w-full h-full content-center items-center">
+              20c - 30c
+            </strong>
+          </div>
+          {/* Medium Temp container */}
+          <div className="flex border-2 border-black w-10 h-10 ">
+            <strong className="w-full h-full content-center items-center">
+              25c
+            </strong>
+          </div>
+        </div>
+        {/* Tomorrow container */}
+        <div className="border-2 m-2 w-auto border-black flex h-28 text-lg justify-evenly content-center items-center">
+          {/* Day container */}
+          <div>
+            <h3>Friday</h3>
+          </div>
+          {/* Image container */}
+          <div className="border-2 border-black w-10 h-10">
+            <img
+              className="w-full h-full content-center items-center"
+              src=""
+              alt=""
+            />
+          </div>
+
+          {/* Min-Max temp container */}
+          <div>
+            <strong className="w-full h-full content-center items-center">
+              20c - 30c
+            </strong>
+          </div>
+          {/* Medium Temp container */}
+          <div className="flex border-2 border-black w-10 h-10 ">
+            <strong className="w-full h-full content-center items-center">
+              25c
+            </strong>
+          </div>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

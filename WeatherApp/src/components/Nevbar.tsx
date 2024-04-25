75352d5589f2fdbdd193e2deacadfde0f8108444
LloydElery <dennisgooglar@gmail.com>
2024-04-25 13:15:00 +0200

    Navbar fix and styling

--- WeatherApp/src/components/Nevbar.tsx
+++ WeatherApp/src/components/Nevbar.tsx
@@ -1,19 +1,20 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useUserLocationStore } from "../stores/useUserLocationStore";
 
 const Navbar = () => {
-  const [weather, setWeather]: any = useState(null);
+  const userPosition = useUserLocationStore((state: any) => state.userLocation);
 
+  useEffect(() => {}, [userPosition]);
   return (
     <>
-      <div className="flex flex-col justify-center content-center flex-wrap">
-        <h1 className="text-xl">World Wide Weather</h1>
+      <div className="flex flex-col justify-center content-center flex-wrap relative top-5">
+        <h1 className="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
+          <span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
+            World Wide <br />
+          </span>{" "}
+          <span className="flex justify-center">Weather</span>
+        </h1>
       </div>
-      {weather && (
-        <div className="flex flex-col justify-center content-center flex-wrap">
-          <h1 className="text-xl">{weather[0].name}</h1>
-        </div>
-      )}
     </>
   );
 };

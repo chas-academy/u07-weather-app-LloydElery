3a2716a64927bb41b3aa09ee7118d2149715af5b
LloydElery <dennisgooglar@gmail.com>
2024-04-25 09:39:30 +0200

    Removed form to fix an error and added a function to disable search when input field is empty

--- WeatherApp/src/components/SearchComponent.tsx
+++ WeatherApp/src/components/SearchComponent.tsx
@@ -13,12 +13,20 @@ const SearchComponent = () => {
 
   const [searchterm, setSearchterm]: any = useState(null); // country/city ...q={country}
   const [weather, setWeather]: any = useState(null); // Setter for API fetch requests
+  const [inputValue, setInputValue]: any = useState("");
 
   const inputRef = useRef<HTMLInputElement | null>(null);
 
   const APIKEY = import.meta.env.VITE_API_KEY_SEARCH; // API key for search (geocoding-api)
 
+  const handleSubmit = () => {
+    if (inputValue.length < 0) {
+      alert("Search for a location:");
+    }
+  };
+
   const searchLocation = async () => {
+    handleSubmit();
     setWeather(null);
     const searchUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${searchterm}&limit=1&appid=${APIKEY}`;
     const response = await fetch(searchUrl);
@@ -52,21 +60,24 @@ const SearchComponent = () => {
       {/* Search container */}
       <div className=" flex justify-center">
         {/* Search input */}
-        <form className=" flex" action="">
-          <div className=" content-center">
-            <input
-              className="primaryButtonSpan  text-white"
-              ref={inputRef}
-              type="text"
-              placeholder="Enter Location"
-            />
-          </div>
-          <div>
-            <button className="primaryButton" onClick={() => searchInput()}>
-              <span className="primaryButtonSpan">Search</span>
-            </button>
-          </div>
-        </form>
+        <div className=" content-center">
+          <input
+            className="primaryButtonSpan  text-white"
+            ref={inputRef}
+            onChange={(e) => setInputValue(e.target.value)}
+            type="text"
+            placeholder="Enter Location"
+          />
+        </div>
+        <div>
+          <button
+            className="primaryButton"
+            disabled={inputValue.length === 0}
+            onClick={() => searchInput()}
+          >
+            <span className="primaryButtonSpan">Search</span>
+          </button>
+        </div>
       </div>
     </>
   );
